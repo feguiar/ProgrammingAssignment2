1965bc69caadca725f68cc66ee3ebc132ec9f29b
feguiar <feguiar@gmail.com>
2016-09-17 14:40:07 -0300

    Assign2

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,7 @@
 ##is is solving the given matrix
 
 ## Write a short comment describing this function
+##This function creates a constraint of functions that set and get the solved matrix, and set and get a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -22,6 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##This function solve de matrix and returns it, if the matrix is already solved (m!=NULL) it returns the cached matrix(m)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
