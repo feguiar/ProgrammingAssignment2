f2eeb9dda1623f0f11713690816b8ed478ae254b
feguiar <feguiar@gmail.com>
2016-09-17 14:32:02 -0300

    Assign2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##The functions were created based on the example of caching the mean of vector, insted of calculating the mean
+##is is solving the given matrix
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolved <- function(solvedMatrix) m <<- solvedMatrix
+  getSolved <- function() m
+  list(set = set, get = get,
+       setSolved = setSolved,
+       getSolved = getSolved)
 }
 
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolved()
+  if(!is.null(m)) {
+    message("getting cached data")
+    m
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolved(m)
+  m
 }
